9c8d50967095f4a1abc83fc07a2216a74c8def75
lucasviner <lucasviner@gmail.com>
2022-05-02 12:28:11 +0200

    Creació Menú principal

--- Menú.gd
+++ Menú.gd
@@ -1,24 +1,43 @@
-extends Control
+extends MarginContainer
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const first_scene = preload("res://Nivell1.tscn")
 
+onready var selector_one = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer/HBoxContainer/Selector
+onready var selector_two = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer3/HBoxContainer/Selector
+onready var selector_three = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer2/HBoxContainer/Selector
 
-# Called when the node enters the scene tree for the first time.
+var current_selection = 0
+
 func _ready():
-	pass # Replace with function body.
+	set_current_selection(0)
 
+func _process(delta):
+	if Input.is_action_just_pressed("ui_down") and current_selection < 2:
+		current_selection += 1
+		set_current_selection(current_selection)
+	elif Input.is_action_just_pressed("ui_up") and current_selection > 0:
+		current_selection -= 1
+		set_current_selection(current_selection)
+	elif Input.is_action_just_pressed("ui_accept"):
+		handle_selection(current_selection)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func handle_selection(_current_selection):
+	if _current_selection == 0:
+		get_parent().add_child(first_scene.instance())
+		queue_free()
+	elif _current_selection == 1:
+		get_tree().change_scene("res://Com jugar.tscn")
+	elif _current_selection == 2:
+		get_tree().quit()
 
-
-func _on_Jugar_pressed():
-	get_tree().change_scene("res://Nivell1.tscn")
-
-
-func _on_Sortir_pressed():
-	get_tree().quit()
+func set_current_selection(_current_selection):
+	selector_one.text = ""
+	selector_two.text = ""
+	selector_three.text = ""
+	if _current_selection == 0:
+		selector_one.text = ">"
+	elif _current_selection == 1:
+		selector_two.text = ">"
+	elif _current_selection == 2:
+		selector_three.text = ">"
