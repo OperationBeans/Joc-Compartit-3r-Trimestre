f44910ec30ec3fb58e5a8303c607fc9d584a7331
lucasviner <lucasviner@gmail.com>
2022-05-16 11:58:59 +0200

    canvis menus

--- Menú.gd
+++ Menú.gd
@@ -1,7 +1,7 @@
 extends MarginContainer
 
 
-const first_scene = preload("res://Nivell1.tscn")
+const first_scene = preload("res://Test Scene.tscn")
 
 onready var selector_one = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer/HBoxContainer/Selector
 onready var selector_two = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/CenterContainer3/HBoxContainer/Selector
