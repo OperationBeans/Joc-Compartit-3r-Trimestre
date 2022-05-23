6162eda24fa35418a6a864641f6785eb7a8214ed
OperationBeans <paumorathau@gmail.com>
2022-05-23 17:22:21 +0200

    egypt

---
+++ FONDOS/MainCharacterEgypt.gd
@@ -0,0 +1,66 @@
+extends KinematicBody2D
+
+var velocitat_base = 300
+var velocitat = Vector2.ZERO
+var gravetat = Vector2.DOWN * 980
+var salt = Vector2.UP * 600
+var already_ate = false
+var dead = false
+var is_attacking = false
+var value = 200
+
+
+
+func _physics_process(delta):
+	velocitat.x=0
+	if $TextureProgress.value <= 0:
+		dead = true
+	if dead == true:
+		$AnimatedSprite.play("dead")
+		var timer = Timer.new()
+		timer.set_one_shot(true)
+		timer.set_wait_time(1)
+		timer.connect("timeout", self, "on_timeout_mort")
+		add_child(timer)
+		timer.start()
+	if Input.is_action_just_pressed("W") and is_on_floor() and is_attacking == false and dead == false:
+		velocitat += salt 
+	elif Input.is_action_pressed("D") and is_attacking == false and dead == false:
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play('move')
+		velocitat += Vector2.RIGHT * velocitat_base  
+	elif Input.is_action_pressed("A") and is_attacking == false and dead == false:
+		velocitat += Vector2.LEFT * velocitat_base  
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play('move')
+	else:
+		velocitat.x = 0
+		if is_attacking == false and dead == false:
+			$AnimatedSprite.play("default")
+	if Input.is_action_just_pressed("attack") and dead == false:
+		$AnimatedSprite.play("attack")
+		is_attacking = true
+		$AttackArea/CollisionShape2D.disabled = false
+		$AttackArea2/CollisionShape2D.disabled = false
+	velocitat += gravetat * delta
+	velocitat = move_and_slide(velocitat, Vector2.UP)
+	Global.pos = global_position
+	
+func _on_AnimatedSprite_animation_finished():
+	if $AnimatedSprite.animation == "attack":
+		$AttackArea/CollisionShape2D.disabled = true
+		$AttackArea2/CollisionShape2D.disabled = true
+		is_attacking = false
+		
+func on_timeout_mort():
+	get_tree().change_scene("res://Escena Mort.tscn")
+
+
+
+func _on_AttackArea_body_entered(body):
+	if body.name == "MainCharacterEgypt":
+		$TextureProgress.value -= 50
+
+func _on_AreaCollision_body_entered(body):
+	if body.name == "MainCharacterEgypt":
+		$TextureProgress.value -= 50
--- FONDOS/Pharaon.gd
+++ FONDOS/Pharaon.gd
@@ -23,6 +23,7 @@ func _physics_process(delta):
 		timer.connect("timeout", self, "on_timeout")
 		add_child(timer)
 		timer.start()
+	
 	elif attacking == true and dead == false:
 		var timer2 = Timer.new()
 		timer2.set_one_shot(true)
@@ -30,24 +31,8 @@ func _physics_process(delta):
 		timer2.connect("timeout", self, "on_timeout2")
 		add_child(timer2)
 		timer2.start()
+	
 	if (Global.pos.x)<position.x and dead == false:
-		velocitat.x = 0
-		velocitat += Vector2.RIGHT * velocitat_base 
-		velocitat += gravetat * delta
-		velocitat = move_and_slide(velocitat, Vector2.UP)
-		if attacking == false:
-			velocitat_base = 250
-			$AnimatedSprite.flip_h = false
-			$AnimatedSprite.play("move")
-			$AttackArea/AttackCollision1.disabled = true
-			$AttackArea/AttackCollision2.disabled = true
-		if attacking == true:
-			velocitat_base = 350
-			$AnimatedSprite.flip_h = false
-			$AnimatedSprite.play("attack")
-			$AttackArea/AttackCollision1.disabled = false
-			$AttackArea/AttackCollision2.disabled = false
-	elif (Global.pos.x)>position.x and dead == false:
 		velocitat.x = 0
 		velocitat += Vector2.LEFT * velocitat_base 
 		velocitat += gravetat * delta
@@ -64,10 +49,24 @@ func _physics_process(delta):
 			$AnimatedSprite.play("attack")
 			$AttackArea/AttackCollision1.disabled = false
 			$AttackArea/AttackCollision2.disabled = false
-	else:
+	elif (Global.pos.x)>position.x and dead == false:
 		velocitat.x = 0
-		$AnimatedSprite.play("default")
-
+		velocitat += Vector2.RIGHT * velocitat_base 
+		velocitat += gravetat * delta
+		velocitat = move_and_slide(velocitat, Vector2.UP)
+		if attacking == false:
+			velocitat_base = 250
+			$AnimatedSprite.flip_h = false
+			$AnimatedSprite.play("move")
+			$AttackArea/AttackCollision1.disabled = true
+			$AttackArea/AttackCollision2.disabled = true
+		if attacking == true:
+			velocitat_base = 350
+			$AnimatedSprite.flip_h = false
+			$AnimatedSprite.play("attack")
+			$AttackArea/AttackCollision1.disabled = false
+			$AttackArea/AttackCollision2.disabled = false
+	
 func on_timeout():
 	attacking = true
 
@@ -75,9 +74,17 @@ func on_timeout2():
 	attacking = false
 
 func _on_AttackArea_body_entered(body):
-	if hits == 1:
-		dead = true
-		$AnimatedSprite.play("dead")
-	else:
-		hits += 1
+	if body.name == "Pharaon":
+		if hits == 1:
+			dead = true
+			$AnimatedSprite.play("dead")
+		else:
+			hits += 1
 
+func _on_AttackArea2_body_entered(body):
+	if body.name == "Pharaon":
+		if hits == 1:
+			dead = true
+			$AnimatedSprite.play("dead")
+		else:
+			hits += 1
