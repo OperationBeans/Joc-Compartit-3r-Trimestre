388fbd264d6824ac83445dcf5c649a405d2a0be3
OperationBeans <101245841+OperationBeans@users.noreply.github.com>
2022-05-25 08:38:30 +0200

    sdgdfsg
    f

--- COBRA.gd
+++ COBRA.gd
@@ -4,22 +4,24 @@ var velocitat_base = 150
 var velocitat = Vector2.ZERO
 var gravetat = Vector2.DOWN * 980
 var dead = false
+var dreta = true
+var esquerra = false
 
 func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	if (Global.pos.x)>position.x and dead == false:
+	if is_on_floor() == true and dreta == true:
 		velocitat.x = 0
 		velocitat += Vector2.RIGHT * velocitat_base 
 		velocitat += gravetat * delta
 		velocitat = move_and_slide(velocitat, Vector2.UP)
-		$AreaEnemy/AnimatedSprite.flip_h = true
-		$AreaEnemy/AnimatedSprite.play("move")
+		$COBRA.flip_h = true
+		$COBRA.play("move")
 	elif (Global.pos.x)<position.x and dead == false:
 		velocitat.x = 0
 		velocitat += Vector2.LEFT * velocitat_base 
 		velocitat += gravetat * delta
 		velocitat = move_and_slide(velocitat, Vector2.UP)
-		$AreaEnemy/AnimatedSprite.flip_h = false
-		$AreaEnemy/AnimatedSprite.play("move")
+		$COBRA.flip_h = false
+		$COBRA.play("move")
