086b79cd1f037ceeeeac21b410855d95f17807dd
rAyZzorr <85404411+rAyZzorr@users.noreply.github.com>
2022-05-29 11:15:21 +0200

    iygdwasdryctfuy

--- Nau_cinem_pr-eg.gd
+++ Nau_cinem_pr-eg.gd
@@ -1,18 +1,13 @@
 extends KinematicBody2D
 
-var velocitat_base = 200
-var velocitat = Vector2.ZERO
-var gravetat = Vector2.RIGHT * 1500
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func _physics_process(delta):
-	velocitat.x = 0
-	velocitat += Vector2.RIGHT * velocitat_base 
-	velocitat += gravetat * delta
-	velocitat = move_and_slide(velocitat, Vector2.UP)
+func _process(delta):
+	position.x += 5
 
 
 func _on_AreaCastellNau_body_entered(body):
